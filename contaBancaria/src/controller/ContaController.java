0f8802d634e000408cf499eb48b69afc647e15fb
Patricia Eliseu <patyeliseu4@gmail.com>
2025-09-25 17:31:54 -0300

    add ContaController

---
+++ contaBancaria/src/controller/ContaController.java
@@ -0,0 +1,68 @@
+package controller;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import conta.bancaria.model.Conta;
+import repository.ContaRepository;
+
+
+
+public class ContaController implements ContaRepository{
+	
+	private List<Conta> listaContas = new ArrayList<Conta>();
+	int numero = 0;
+
+	@Override
+	public void listarTodas() {
+		for(var conta : listaContas) {
+			conta.visualizar();
+		}
+		
+	}
+
+	@Override
+	public void cadastrar(Conta conta) {
+		listaContas.add(conta);
+		System.out.println("Conta Cadastrada com sucesso!");
+	}
+
+	@Override
+	public void atualizar(Conta conta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procurarPorNumero(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sacar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void depositar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
